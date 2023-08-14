236bb2ec9f7e3c9b43cca9fc61a456b5f6ca30c6
Frannerz <francesstephens@hotmail.com>
2023-08-14 22:03:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetSection = document.querySelector(link.getAttribute('href'));
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
